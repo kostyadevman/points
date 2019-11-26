3dae0825d8d0b3d64fecd05a82510225b3a60a66
jora <jora@email.ru>
2019-11-26 22:37:23 +0300

    initial

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,78 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+  <q-layout view="lhh lpr lff">
+    <q-header elevated class="glossy">
+      <q-toolbar>
+        <q-btn
+          flat
+          dense
+          round
+          @click="leftDrawerOpen = !leftDrawerOpen"
+          aria-label="Menu"
+          icon="menu"
+        />
+
+        <q-toolbar-title>
+          Quasar App
+        </q-toolbar-title>
+
+        <div>Quasar v{{ $q.version }}</div>
+      </q-toolbar>
+    </q-header>
+
+    <q-drawer
+      v-model="leftDrawerOpen"
+      bordered
+      content-class="bg-grey-2"
+    >
+
+    </q-drawer>
+
+    <q-page-container>
+      <yandex-map :settings="settings"
+          :coords="coords"
+          @click="onClick"
+        >
+          <ymap-marker
+            :coords="coords"
+            marker-id="123"
+            hint-content="some hint"
+          />
+        </yandex-map>
+    </q-page-container>
+  </q-layout>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import { yandexMap, ymapMarker  } from 'vue-yandex-maps'
 
 export default {
-  name: 'app',
+  name: 'LayoutDefault',
+
   components: {
-    HelloWorld
+      yandexMap,
+      ymapMarker
+
+  },
+
+  data () {
+    return {
+      leftDrawerOpen: this.$q.platform.is.desktop,
+      settings: {
+          apiKey: 'KEY',
+          lang: 'ru_RU',
+          coordorder: 'latlong',
+          version: '2.1'
+        },
+      coords: [54, 39]
+    }
+  },
+  methods: {
+    onClick(e) {
+      this.coords = e.get('coords');
+    }
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
 </style>
